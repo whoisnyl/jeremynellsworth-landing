1bb020980c9ee44e025d0e878f1c7d62a58dd000
jerekel87 <j3r0m3_118@hotmail.com>
2022-07-04 15:24:53 +0800

    Added vehicle wrap sample

--- _mocks_/services.js
+++ _mocks_/services.js
@@ -10,6 +10,32 @@ import signage from "../public/images/services/signage.png";
 import productPackage from "../public/images/services/product-packaging.png";
 // services sample
 import elrinconTacos from "../public/images/creatives/el-rincon-tacos/el-rincon-tacos.png";
+import greenskullWingbones from "../public/images/creatives/green-skull-wingbones/green-skull-wingbones.png";
+import inspireLegacy from "../public/images/creatives/inspire/inspire-legacy.png";
+import gotdaMucnhies from "../public/images/creatives/got-da-munchies/got-da-munchies.png";
+import stagandSons from "../public/images/creatives/stagg-and-sons/stagg-and-sons.png";
+import iglesiaCristiana from "../public/images/creatives/iglesia-cristiana/iglesia-cristiana.png";
+import smacknCheeze from "../public/images/creatives/smack-n-cheeze/smack-n-cheeze.png";
+import frostyDesertllc from "../public/images/creatives/frosty-desert-llc/frosty-desert-llc.png";
+import fatboysBbq from "../public/images/creatives/fat-boys-bbq/fat-boys-bbq.png";
+import polaniPoised from "../public/images/creatives/polani-poised/polani-poised.png";
+import shadybrushClearing from "../public/images/creatives/shady-brush-clearing/shady-brush-clearing.png";
+import rapidTire from "../public/images/creatives/rapid-tire/rapid-tire.png";
+import venetianRemodeling from "../public/images/creatives/venetian-remodeling/venetian-remodeling.png";
+import kajunKoatings from "../public/images/creatives/kajun-koatings/kajun-koatings.png";
+import slappie from "../public/images/creatives/slappie/slappie.png";
+import tenderBonez from "../public/images/creatives/tender-bonez/tender-bonez.png";
+import diaz from "../public/images/creatives/diaz/diaz.png";
+import freshPic from "../public/images/creatives/fresh-pic/fresh-pic.png";
+import steamerPros from "../public/images/creatives/steamer-pros/steamer-pros.png";
+import burrerKing from "../public/images/creatives/burrer-king/burrer-king.png";
+import grenadaLake from "../public/images/creatives/grenada-lake/grenada-lake.png";
+import pyramidConstruction from "../public/images/creatives/pyramid-construction/pyramid-construction.png";
+import leagueOf from "../public/images/creatives/league-of/league-of.png";
+import oldLine from "../public/images/creatives/old-line/old-line.png";
+import kingsDonuts from "../public/images/creatives/kings-donuts/kings-donuts.png";
+import sawtoothStructures from "../public/images/creatives/sawtooth-structure/sawtooth-structure.png";
+import frostyDesert from "../public/images/creatives/frosty-desert/frosty-desert.png";
 
 export const servicesSample = [
   {
@@ -17,6 +43,162 @@ export const servicesSample = [
     category: "brand",
     theme: "#e31f22",
     logo: elrinconTacos,
+  },
+  {
+    title: "Got Da Munchies?",
+    category: "brand",
+    theme: "#0e9100",
+    logo: gotdaMucnhies,
+  },
+  {
+    title: "Green Skull Wingbones",
+    category: "brand",
+    theme: "#c3d995",
+    logo: greenskullWingbones,
+  },
+  {
+    title: "Stagg and Sons",
+    category: "brand",
+    theme: "#031337",
+    logo: stagandSons,
+  },
+  {
+    title: "Smack N Cheeze",
+    category: "brand",
+    theme: "#fec70b",
+    logo: smacknCheeze,
+  },
+  {
+    title: "Frosty Desert LLC",
+    category: "brand",
+    theme: "#2477c1",
+    logo: frostyDesertllc,
+  },
+  {
+    title: "Fat Boys BBQ",
+    category: "brand",
+    theme: "#e9cb2b",
+    logo: fatboysBbq,
+  },
+  {
+    title: "Pōlani Poised",
+    category: "brand",
+    theme: "#c0d1a1",
+    logo: polaniPoised,
+  },
+  {
+    title: "Iglesia",
+    category: "brand",
+    theme: "#091a48",
+    logo: iglesiaCristiana,
+  },
+  {
+    title: "Shady Brush Clearing",
+    category: "brand",
+    theme: "#2c891b",
+    logo: shadybrushClearing,
+  },
+  {
+    title: "Rapid Tire",
+    category: "brand",
+    theme: "#121212",
+    logo: rapidTire,
+  },
+  {
+    title: "Venetian Remodeling Group",
+    category: "brand",
+    theme: "#c16f36",
+    logo: venetianRemodeling,
+  },
+  {
+    title: "Tender Bonez BBQ",
+    category: "brand",
+    theme: "#ec1500",
+    logo: tenderBonez,
+  },
+  {
+    title: "Kajun Koatings LLC",
+    category: "brand",
+    theme: "#91cd2c",
+    logo: kajunKoatings,
+  },
+  {
+    title: "Slappie",
+    category: "brand",
+    theme: "#e02305",
+    logo: slappie,
+  },
+  {
+    title: "Díaz Quality Gutters Llc",
+    category: "brand",
+    theme: "#0b4b88",
+    logo: diaz,
+  },
+  {
+    title: "Fresh Pic 360",
+    category: "brand",
+    theme: "#202539",
+    logo: freshPic,
+  },
+  {
+    title: "Pyramid Construction",
+    category: "brand",
+    theme: "#18120b",
+    logo: pyramidConstruction,
+  },
+  {
+    title: "Steamer Pros",
+    category: "brand",
+    theme: "#0079bf",
+    logo: steamerPros,
+  },
+  {
+    title: "League Of The Future Vending and Catering",
+    category: "brand",
+    theme: "#002c50",
+    logo: leagueOf,
+  },
+  {
+    title: "Burrer King",
+    category: "brand",
+    theme: "#b5012a",
+    logo: burrerKing,
+  },
+  {
+    title: "Grenada Lake Resort, LLC",
+    category: "brand",
+    theme: "#0b342c",
+    logo: grenadaLake,
+  },
+  {
+    title: "Old Line Auto Pros, LLC",
+    category: "brand",
+    theme: "#231f20",
+    logo: oldLine,
+  },
+  {
+    title: "Inspire Legacy Group",
+    category: "brand",
+    theme: "#111111",
+    logo: inspireLegacy,
+  },
+  {
+    title: "Kings Donuts",
+    category: "brand",
+    theme: "#2e0392",
+    logo: kingsDonuts,
+  },
+  {
+    title: "Sawtooth Structures",
+    category: "brand",
+    theme: "#0f1c35",
+    logo: sawtoothStructures,
+  },
+  {
+    title: "Frosty Desert LLC",
+    category: "vehicle-wrap",
+    theme: "#2477c1",
+    logo: frostyDesert,
   },
 ];
 
@@ -28,7 +210,7 @@ const services = [
     color: "#263160",
     details:
       "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.",
-    url: "https://facebook.com",
+    url: "https://jeremyellsworth.typeform.com/to/IZqweahE",
   },
   {
     id: "vehicle-wrap",
--- src/CatalogCard.js
+++ src/CatalogCard.js
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
       transform: "translate(-50%,-50%)",
 
       [theme.breakpoints.up("md")]: {
-        width: "55%",
+        width: "58%",
       },
     },
 
