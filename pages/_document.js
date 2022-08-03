824ccc3eeb6b5ad4e85829bc99049d000b1a3ccc
jerekel87 <j3r0m3_118@hotmail.com>
2022-08-04 00:07:23 +0800

    move gtag script to index

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Script from 'next/script'
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
 import theme from "../src/theme";
@@ -10,9 +9,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-        <Script id="gtag" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M82FF5P');`}
-        </Script>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="shortcut icon" href="/favicon.png" />
@@ -32,9 +28,6 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <noscript>
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M82FF5P" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-          </noscript>
         </body>
       </Html>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Head from "next/head";
+import Script from 'next/script'
 // mui
 import Box from "@mui/material/Box";
 // components
@@ -60,8 +61,14 @@ export default function Index() {
     <React.Fragment>
       <Header elemRef={headerRef} navigate={handleNavigate} />
       <Head>
+        <Script id="gtag" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M82FF5P');`}
+        </Script>
         <title>Jeremy Ellsworth</title>
       </Head>
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M82FF5P" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+      </noscript>
       <Box component="main">
         <TopContent />
         <Services elemRef={servicesRef} />
