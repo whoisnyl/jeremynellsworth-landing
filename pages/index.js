c4958cc0580ee2b3b59e68d284ac09b27a685e16
jerekel87 <j3r0m3_118@hotmail.com>
2022-08-04 00:11:23 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -61,11 +61,11 @@ export default function Index() {
     <React.Fragment>
       <Header elemRef={headerRef} navigate={handleNavigate} />
       <Head>
-        <Script id="gtag" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M82FF5P');`}
-        </Script>
         <title>Jeremy Ellsworth</title>
       </Head>
+      <Script id="gtag" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M82FF5P');`}
+      </Script>
       <noscript>
         <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M82FF5P" height="0" width="0" style="display:none;visibility:hidden"></iframe>
       </noscript>
