4669bba6d2d3142a0cedc7e93440ac2ca3a7ee8a
jerekel87 <j3r0m3_118@hotmail.com>
2022-08-03 23:59:13 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,9 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-        <Script id="gtag" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M82FF5P');`}}></Script>
+        <Script id="gtag" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M82FF5P');`}
+        </Script>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="shortcut icon" href="/favicon.png" />
@@ -28,11 +30,11 @@ export default class MyDocument extends Document {
           {this.props.emotionStyleTags}
         </Head>
         <body>
+          <Main />
+          <NextScript />
           <noscript>
             <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M82FF5P" height="0" width="0" style="display:none;visibility:hidden"></iframe>
           </noscript>
-          <Main />
-          <NextScript />
         </body>
       </Html>
     );
