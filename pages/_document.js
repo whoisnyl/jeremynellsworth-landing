9a734c1d37419b8d2566806986890e217d7c4e79
jerekel87 <j3r0m3_118@hotmail.com>
2022-08-03 23:45:13 +0800

    added gtag nabager

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Script from 'next/script'
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
 import theme from "../src/theme";
@@ -9,6 +10,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+        <Script  strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M82FF5P');`}}></Script>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="shortcut icon" href="/favicon.png" />
@@ -26,6 +28,9 @@ export default class MyDocument extends Document {
           {this.props.emotionStyleTags}
         </Head>
         <body>
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M82FF5P" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
